eb07d50149c0f65b5f7039f4cab6a62e71605faf
Satyakam Maurya <Satyakammaurya@gmail.com>
2025-07-05 16:04:49 +0530

    one more changes i did

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import AppRoutes from './routes/AppRoutes';
 
+
+
+// for now just see the code - and analysis karte rha karo every week do chaar baar bhi dekhte rhe to aa hi jayega with time
+// backend python se try krne ka try kr sakte but uska alag repo rakhenge ya see if isi me seperate folder banake ho jaaye to
+
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const sidebarRef = useRef(null); // ✅ create ref for sidebar
