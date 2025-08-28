4756c8115c906648adbe8837b73d9495c3587966
Satyakam Maurya <Satyakammaurya@gmail.com>
2025-08-28 12:00:34 +0530

    did some small changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,14 @@ import "./Cart.css";
 export default function Cart() {
   return (
     <div className="cart-container">
+      {/* Left Side - Suggested Items */}
+      <div className="cart-suggestions">
+        <h2>Similar Items from Your Search</h2>
+        <div className="suggestion-box">
+          <p>(Suggested items will be displayed here)</p>
+        </div>
+      </div>
+
       {/* Right Side - Your Orders */}
       <div className="cart-orders">
         <h2>Your Orders</h2>
@@ -10,14 +18,6 @@ export default function Cart() {
           <p>(Your order details will appear here)</p>
         </div>
         <button className="proceed-btn">Click to Proceed</button>
-      </div>
-
-      {/* Left Side - Suggested Items */}
-      <div className="cart-suggestions">
-        <h2>Similar Items from Your Search</h2>
-        <div className="suggestion-box">
-          <p>(Suggested items will be displayed here)</p>
-        </div>
       </div>
     </div>
   );
