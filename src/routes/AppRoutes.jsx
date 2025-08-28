1292cb5aa91986f43bab766dcf366281f6764358
Satyakam Maurya <Satyakammaurya@gmail.com>
2025-08-28 09:29:20 +0530

    added basic contact page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home';
 import Option1 from '../pages/Option1';
 import Option2 from '../pages/Option2';
 import Option3 from '../pages/Option3';
+import Contact from '../pages/Contact'; // ✅ Added Contact
 
 // Add Option4, Option5
 
@@ -13,6 +14,7 @@ export default function AppRoutes() {
       <Route path="/option1" element={<Option1 />} />
       <Route path="/option2" element={<Option2 />} />
       <Route path="/option3" element={<Option3 />} />
+      <Route path="/contact" element={<Contact />} /> {/* ✅ Added Contact route */}
 
       {/* Add the rest similarly */}
     </Routes>
